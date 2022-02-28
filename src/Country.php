395df378256d65494873620cae97082fef988808
James <james@sglms.com>
2022-02-28 17:36:41 -0300

    Initial Commit

---
+++ src/Country.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SGLMS\CountryList;
+
+class Country
+{
+    public static function all(): array
+    {
+        return [1,2,3];
+    }
+}
---
+++ tests/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use SGLMS\CountryList\Country;
+
+require_once "../src/Country.php";
+
+echo "OO";
+echo $countries = Country::all();
+
